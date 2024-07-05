c8cf04cd776546f23f9c58627bf6fd54c3ed680a
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-07-05 15:12:36 -0500

    eks

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -28,3 +28,11 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = "/aws/eks/my-cluster/cluster"
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
